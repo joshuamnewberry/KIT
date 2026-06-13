dfa133a863e88336c35b1d427531fa553b748f37
evanusernametaken <ejlogan091407@GMAIL.COM>
2026-06-12 22:57:52 -0400

    Refactor ContactsViewModel to use KITRepository

--- shared/src/commonMain/kotlin/edu/gvsu/cis/kit/viewModels/ContactsViewModel.kt
+++ shared/src/commonMain/kotlin/edu/gvsu/cis/kit/viewModels/ContactsViewModel.kt
@@ -1,10 +1,76 @@
 package edu.gvsu.cis.kit.viewModels
 
 import androidx.lifecycle.ViewModel
-import edu.gvsu.cis.kit.data.AppDAO
+import androidx.lifecycle.viewModelScope
+import edu.gvsu.cis.kit.data.Contact
+import edu.gvsu.cis.kit.data.ImportantDate
+import edu.gvsu.cis.kit.data.ImportantDateType
+import edu.gvsu.cis.kit.data.KITRepository
+import edu.gvsu.cis.kit.data.ReminderFrequency
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class ContactsViewModel(
-    private val dao: AppDAO
+    private val repository: KITRepository
 ) : ViewModel() {
 
+    private val _contacts = MutableStateFlow<List<Contact>>(emptyList())
+    val contacts: StateFlow<List<Contact>> = _contacts
+
+    private val _importantDates = MutableStateFlow<List<ImportantDate>>(emptyList())
+    val importantDates: StateFlow<List<ImportantDate>> = _importantDates
+
+    fun loadContacts() {
+        viewModelScope.launch {
+            _contacts.value = repository.getAllContacts()
+        }
+    }
+
+    fun addContact(
+        name: String,
+        phoneNumber: String,
+        email: String,
+        relationshipType: String
+    ) {
+        viewModelScope.launch {
+            repository.addContact(
+                name = name,
+                phoneNumber = phoneNumber.ifBlank { null },
+                email = email.ifBlank { null },
+                relationshipType = relationshipType.ifBlank { null }
+            )
+            loadContacts()
+        }
+    }
+
+    fun addWeeklyReminder(contactId: String) {
+        viewModelScope.launch {
+            repository.addReminder(contactId, ReminderFrequency.WEEKLY)
+        }
+    }
+
+    fun addMonthlyReminder(contactId: String) {
+        viewModelScope.launch {
+            repository.addReminder(contactId, ReminderFrequency.MONTHLY)
+        }
+    }
+
+    fun addQuarterlyReminder(contactId: String) {
+        viewModelScope.launch {
+            repository.addReminder(contactId, ReminderFrequency.QUARTERLY)
+        }
+    }
+
+    fun addBirthday(contactId: String, title: String, dateMillis: Long) {
+        viewModelScope.launch {
+            repository.addImportantDate(
+                contactId = contactId,
+                title = title,
+                type = ImportantDateType.BIRTHDAY,
+                dateMillis = dateMillis
+            )
+            _importantDates.value = repository.getImportantDatesForContact(contactId)
+        }
+    }
 }
