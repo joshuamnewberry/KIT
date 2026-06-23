8ff308000223e73d08ea81fa35b21339b1ee9f08
evanusernametaken <ejlogan091407@GMAIL.COM>
2026-06-23 11:26:45 -0400

    Update MainViewController.kt

--- shared/src/iosMain/kotlin/edu/gvsu/cis/kit/MainViewController.kt
+++ shared/src/iosMain/kotlin/edu/gvsu/cis/kit/MainViewController.kt
@@ -1,5 +1,11 @@
 package edu.gvsu.cis.kit
 
 import androidx.compose.ui.window.ComposeUIViewController
+import edu.gvsu.cis.kit.data.getDatabaseInstance
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val database = getDatabaseInstance(getDatabaseBuilder())
+    initKoin(database.getDao())
+
+    App()
+}
