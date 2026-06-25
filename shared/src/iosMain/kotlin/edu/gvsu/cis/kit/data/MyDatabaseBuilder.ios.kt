7e98d90e29ada353ea5c2b869f94d6593c785840
evanusernametaken <ejlogan091407@GMAIL.COM>
2026-06-24 22:03:47 -0400

    Update MyDatabaseBuilder to use actual override

--- shared/src/iosMain/kotlin/edu/gvsu/cis/kit/data/MyDatabaseBuilder.ios.kt
+++ shared/src/iosMain/kotlin/edu/gvsu/cis/kit/data/MyDatabaseBuilder.ios.kt
@@ -4,7 +4,7 @@ import androidx.room.RoomDatabaseConstructor
 import edu.gvsu.cis.kit.getDatabaseBuilder
 
 actual object MyDatabaseBuilder : RoomDatabaseConstructor<AppDB> {
-    override fun initialize(): AppDB {
+    actual override fun initialize(): AppDB {
         return getDatabaseInstance(getDatabaseBuilder())
     }
 }
